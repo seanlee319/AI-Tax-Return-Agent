255c3842c26d3b9121186ada90639e801cee32e2
seanlee319 <lysean0319@gmail.com>
2025-07-23 15:07:13 -0700

    new port

--- backend/main.py
+++ backend/main.py
@@ -691,4 +691,4 @@ def serve_output_file(filename):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
